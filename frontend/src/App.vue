b3ea7d11813edb9e7e5ea9ce3820659be7b220f4
AbderrahmaneBouzemlal <AbderrahmaneBouzemlal@gmail.com>
2025-07-01 12:45:55 +0800

    set the url to match the backend endpoint

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted } from "vue";
 
-let url = `https://flask-backend-591086055772.us-central1.run.app`;
+let url = `/api`;
 
 const rows = ref([]);
 const values = ref({});
